2879b54f94328dae3b760c3fbc8fda7568f24567
Semptify Dev <dev@semptify.org>
2026-07-12 19:58:04 -0500

    chore: remove orphan static/data/css directory and temp stub_fix files

--- tmp_read.py
+++
@@ -1,5 +0,0 @@
-import pathlib, sys
-path = pathlib.Path(sys.argv[1])
-text = path.read_text(encoding='utf-8')
-out = pathlib.Path('stub_fix_summary.json')
-out.write_text(text[:8000], encoding='utf-8')
